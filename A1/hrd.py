fdb7ac76869c1fe970281922bc986a651b655a13
Shadman Kaif <kaifshadman@gmail.com>
2022-09-22 15:00:19 -0400

    Added vertical piece finder and Manhattan cost functions

--- A1/hrd.py
+++ A1/hrd.py
@@ -1,6 +1,10 @@
 import sys
 import time
 
+# Create global variables for the number of columns and rows of the board
+cols = 5
+rows = 4
+
 start_time = time.time()
 
 # Set the three parameters to pass in
@@ -8,26 +12,61 @@ filename = sys.argv[1]
 dfs_filename = sys.argv[2]
 astar_filename = sys.argv[3]
 
-# Create a 4x5 array to store the digits of the puzzle
-puzzle = [[] for x in range(5)]
+# Create a 5x4 array to store the digits of the puzzle
+puzzle = [[] for x in range(cols)]
 
 # Import the puzzle.txt file and use a nested for loop to store digits
 with open(filename) as file:
-    for i in range(5):
-        for j in range(4):
+    for i in range(cols):
+        for j in range(rows):
             puzzle[i].append(file.read(1))
         file.read(1)
 
 print(puzzle)
 
-# Function that finds the coordinates of state and value of a specific digit
-def coordinates_locater(state, value):
+# Function that finds the coordinates of a specific digit given the configuration
+def coordinates_locater(digit, config):
     index = []
-    for i in range(5):
-        for j in range(4):
-            if state[i][j] == value:
+    for i in range(cols):
+        for j in range(rows):
+            if config[i][j] == digit:
                 index.append((i, j))
     return index
+
+# Function to find all the vertical pieces
+# This is needed as there are 32 initial configurations
+# and because the integers except 1 and 0 are randomized
+def vertical_pieces(config):
+    digits_vertical = []
+    # 2
+    coordinates_2 = coordinates_locater('2', config)
+    if coordinates_2[0][1] == coordinates_2[1][1]:
+        digits_vertical.append('2')
+    # 3
+    coordinates_3 = coordinates_locater('3', config)
+    if coordinates_3[0][1] == coordinates_3[1][1]:
+        digits_vertical.append('3')   
+    # 4
+    coordinates_4 = coordinates_locater('4', config)
+    if coordinates_4[0][1] == coordinates_4[1][1]:
+        digits_vertical.append('4')
+    # 5
+    coordinates_5 = coordinates_locater('5', config)
+    if coordinates_5[0][1] == coordinates_5[1][1]:
+        digits_vertical.append('5')
+    # 6
+    coordinates_6 = coordinates_locater('6', config)
+    if coordinates_6[0][1] == coordinates_6[1][1]:
+        digits_vertical.append('6')
+    
+    return digits_vertical
+
+# Simple Manhattan distance function
+def manhattan_distance(config):
+    index_of_ones = coordinates_locater('1', config)
+    leftmost_x_pos, leftmost_y_pos = index_of_ones[0][1], index_of_ones[0][0]
+    manhattan_cost = abs(1-leftmost_x_pos) + (3-leftmost_y_pos)
+    return manhattan_cost
 
 with open(dfs_filename, "w" ) as dfs_f:
     print("Hey", file=dfs_f)
@@ -40,4 +79,11 @@ final_time = end_time - start_time
 
 print(final_time)
 
-print(coordinates_locater(puzzle, '1'))
+print(coordinates_locater('1', puzzle))
+
+vertical_blocks = vertical_pieces(puzzle)
+print(vertical_blocks)
+
+print(manhattan_distance(puzzle))
+
+print(puzzle)
