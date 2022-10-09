4d58a96ec5af4ce0f241b7be775e26028bf40f14
Shadman Kaif <kaifshadman@gmail.com>
2022-10-09 17:46:16 -0400

    Finished single jump successor function

--- A2/checkers.py
+++ A2/checkers.py
@@ -1,4 +1,5 @@
 # Import libraries
+from distutils.command.config import config
 import sys
 import copy
 
@@ -11,13 +12,13 @@ rows = 8
 columns = 8
 
 # Create a list of list for the board
-board = [[] for x in range(rows)]
+configuration = [[] for x in range(rows)]
 
 # Import the input.txt file and use a nested for loop to store characters
 with open(input_file) as file:
     for i in range(rows):
         for j in range(columns):
-            board[i].append(file.read(1))
+            configuration[i].append(file.read(1))
         file.read(1)
 
 # Function that finds the coordinates of a specific character given the configuration
@@ -29,89 +30,163 @@ def coordinates_locater(piece, config):
                 index.append((i, j))
     return index
 
-# Jump Function
-def jump(config, player):
-    new_jump = []
-    if player == 'b':
-        new_jump = [1]
-    if player == 'r':
-        new_jump = [2]
-    return new_jump
+def jump(board, player):
+    path = []
+    player_piece = coordinates_locater(player, board)
+    print("Red piece coordinates: " + str(player_piece))
+
+    # Create successors for the player piece
+    for i in player_piece:
+        if player == 'r':
+            # Jump left
+            if i[0] <= 5 and i[1] >= 2 and board[i[0]+2][i[1]-2] == '.' and (board[i[0]+1][i[1]-1] == 'b' or board[i[0]+1][i[1]-1] == 'B'):
+                new_board = copy.deepcopy(board)
+                new_board[i[0]][i[1]] = '.'
+                new_board[i[0]+1][i[1]-1] = '.'
+                if i[0] == 5:
+                    new_board[i[0]+2][i[1]-2] = 'R'
+                else: 
+                    new_board[i[0]+2][i[1]-2] = 'r'
+                path.append(new_board)
+
+            # Jump right
+            if i[0] <= 5 and i[1] <= 5 and board[i[0]+2][i[1]+2] == '.' and (board[i[0]+1][i[1]+1] == 'b' or board[i[0]+1][i[1]+1] == 'B'):
+                new_board = copy.deepcopy(board)
+                new_board[i[0]][i[1]] = '.'
+                new_board[i[0]+1][i[1]+1] = '.'
+                if i[0] == 5:
+                    new_board[i[0]+2][i[1]+2] = 'R'
+                else: 
+                    new_board[i[0]+2][i[1]+2] = 'r'
+                path.append(new_board)
+
+    return path
 
 # Successor function
-def successor(config):
+def successor(board, player):
     path = []
-    red_kings = coordinates_locater('R', config)
-    red_pieces = coordinates_locater('r', config)
-    print("Red piece coordinates: " + str(red_pieces))
-    
-    # Create successors for the single red piece
-    for i in red_pieces:
-        # Left Diagonal jump when not becoming King
-        if i[0] > 2 and i[0] < 8 and i[1] > 1 and i[1] <= 7:
-            if (config[i[0]-1][i[1]-1] == 'b' or config[i[0]-1][i[1]-1] == 'B'):
-                updated_config = copy.deepcopy(config)
-                updated_config[i[0]-1][i[1]-1] = '.'
-                updated_config[i[0]][i[1]] = '.'
-                updated_config[i[0]-2][i[1]-2] = 'r'
-                path.append(updated_config)
-            
-            # Left diagonal
-            elif (config[i[0]-1][i[1]-1] == '.'):
-                updated_config = copy.deepcopy(config)
-                updated_config[i[0]-1][i[1]-1] = 'r'
-                updated_config[i[0]][i[1]] = '.'
-                path.append(updated_config)
-        
-        # Right Diagonal jump when not becoming King
-        elif i[0] > 2 and i[0] < 8 and i[1] >= 0 and i[1] < 6: 
-            if (config[i[0]-1][i[1]+1] == 'b' or config[i[0]-1][i[1]+1] == 'B'):
-                updated_config = copy.deepcopy(config)
-                updated_config[i[0]-1][i[1]+1] = '.'
-                updated_config[i[0]][i[1]] = '.'
-                updated_config[i[0]-2][i[1]+2] = 'r'
-                break
-            
-            elif(config[i[0]-1][i[1]+1] == '.'):
-                updated_config = copy.deepcopy(config)
-                updated_config[i[0]-1][i[1]+1] = 'r'
-                updated_config[i[0]][i[1]] = '.'
-                break
+    if player == 'r':
+        player_piece = coordinates_locater(player, board)
+        print(player_piece)
+        for i in player_piece:
+            y, x = i[0], i[1]
+            print(y,x)
+            if board[y][x] == 'r':
+                # Right top diagonal single jump
+                if x <= 6 and board[y-1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    if y == 1:
+                        new_board[y-1][x+1] = 'R'
+                    else:
+                        new_board[y-1][x+1] = 'r'
+                    path.append(new_board)
+                # Left top diagonal single jump
+                if x >= 1 and board[y-1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    if y == 1:
+                        new_board[y-1][x-1] = 'R'
+                    else:
+                        new_board[y-1][x-1] = 'r'
+                    path.append(new_board)
+    elif player == 'R':
+        player_piece = coordinates_locater(player, board)
+        print(player_piece)
+        for i in player_piece:
+            y, x = i[0], i[1]
+            print(y,x)
+            if board[y][x] == 'R':
+                # Right top diagonal single jump
+                if x <= 6 and y >= 1 and board[y-1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y-1][x+1] = 'R'
+                    path.append(new_board)
+                # Left top diagonal single jump
+                if x >= 1 and y >= 1 and board[y-1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y-1][x-1] = 'R'
+                    path.append(new_board)
+                # Right down diagonal single jump
+                if x <= 6 and y <= 6 and board[y+1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y+1][x+1] = 'R'
+                    path.append(new_board)
+                # Left down diagonal single jump
+                if x >= 1 and y <= 6 and board[y+1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y+1][x-1] = 'R'
+                    path.append(new_board)
+    elif player == 'b':
+        player_piece = coordinates_locater(player, board)
+        print(player_piece)
+        for i in player_piece:
+            y, x = i[0], i[1]
+            print(y,x)
+            if board[y][x] == 'b':
+                # Right down diagonal single jump
+                if x <= 6 and y <= 6 and board[y+1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    if y == 6:
+                        new_board[y+1][x+1] = 'B'
+                    else: 
+                        new_board[y+1][x+1] = 'b'
+                    path.append(new_board)
+                # Left down diagonal single jump
+                if x >= 1 and y <= 6 and board[y+1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    if y == 6:
+                        new_board[y+1][x-1] = 'B'
+                    else:
+                        new_board[y+1][x-1] = 'b'
+                    path.append(new_board)
+    elif player == 'B':
+        player_piece = coordinates_locater(player, board)
+        print(player_piece)
+        for i in player_piece:
+            y, x = i[0], i[1]
+            print(y,x)
+            if board[y][x] == 'B':
+                # Right top diagonal single jump
+                if x <= 6 and y >= 1 and board[y-1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y-1][x+1] = 'B'
+                    path.append(new_board)
+                # Left top diagonal single jump
+                if x >= 1 and y >= 1 and board[y-1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y-1][x-1] = 'B'
+                    path.append(new_board)
+                # Right down diagonal single jump
+                if x <= 6 and y <= 6 and board[y+1][x+1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y+1][x+1] = 'B'
+                    path.append(new_board)
+                # Left down diagonal single jump
+                if x >= 1 and y <= 6 and board[y+1][x-1] == '.':
+                    new_board = copy.deepcopy(board)
+                    new_board[y][x] = '.'
+                    new_board[y+1][x-1] = 'B'
+                    path.append(new_board)
+    return path
 
-        # Left Diagonal jump when becoming King
-        elif i[0] == 2 and i[1] > 1 and i[1] <= 7:
-            if (config[i[0]-1][i[1]-1] == 'b' or config[i[0]-1][i[1]-1] == 'B'):
-                config[i[0]-1][i[1]-1] = '.'
-                config[i[0]][i[1]] = '.'
-                config[i[0]-2][i[1]-2] = 'R'
-                break
+print(len(successor(configuration, 'r')))
+print(len(successor(configuration, 'R')))
+print(len(successor(configuration, 'b')))
+print(len(successor(configuration, 'B')))
 
-        # Right Diagonal jump when becoming King
-        elif i[0] == 2 and i[1] >= 0 and i[1] < 6:
-            if (config[i[0]-1][i[1]+1] == 'b' or config[i[0]-1][i[1]+1] == 'B'):
-                config[i[0]-1][i[1]+1] = '.'
-                config[i[0]][i[1]] = '.'
-                config[i[0]-2][i[1]+2] = 'R'
-                break
-    
-    return config
-
-successor_nodes = successor(board)
-print(successor_nodes)
-red_pieces = coordinates_locater('r', board)
-print("Red piece coordinates: " + str(red_pieces))
 # Output the file
 output = open(output_file, "w")
 for i in range(rows):
     for j in range(columns):
-        output.write(str(board[i][j]))
+        output.write(str(configuration[i][j]))
     output.write("\n")
-
-
-def Copyboard(board):
-    new_board = [[]]*8
-    for i in range(8):
-        new_board[i] = [] + board[i]
-    return new_board
-
-print(Copyboard(board))
